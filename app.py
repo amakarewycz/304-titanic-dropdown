06fc039f40a76a1da3db3b838d3b102be9edf96d
Alex Makarewycz <alexander_makarewycz@intuit.com>
2022-03-22 08:18:22 +0000

    added Male continuous variable, aand bucketed age categorical variable; switched the plot to show bucketed age by embarkation

--- app.py
+++ app.py
@@ -13,15 +13,26 @@ tabtitle = 'Titanic!'
 color1='#92A5E8'
 color2='#8E44AD'
 color3='#FFC300'
+color4='#37C300'
+
 sourceurl = 'https://www.kaggle.com/c/titanic'
-githublink = 'https://github.com/plotly-dash-apps/304-titanic-dropdown'
+githublink = 'https://github.com/amakarewycz/304-titanic-dropdown'
 
 
 ###### Import a dataframe #######
 df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
 df['Female']=df['Sex'].map({'male':0, 'female':1})
+df['Male']=df['Sex'].map({'male':1, 'female':0})
 df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
-variables_list=['Survived', 'Female', 'Fare', 'Age']
+variables_list=['Survived', 'Female', 'Male', 'Fare', 'Age']
+
+# Create a list with those 5 values as cut points. (props if you can do this without hard-coding them!)
+mybins=[0,18,30,60,80]
+
+# Create some labels for the new variable. NOTE: There are 5 cut points but only four labels. Why is that?
+mylabels=['1 children', '2 young adult', '3 middle-aged', '4 elderly']
+# Use the .cut method to create a new variable using those cut points and labels.
+df['Age Groups']= pd.cut(df['Age'], bins=mybins, labels=mylabels)
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -31,6 +42,7 @@ app.title=tabtitle
 
 ####### Layout of the app ########
 app.layout = html.Div([
+    html.H1('The Titanic:'),
     html.H3('Choose a continuous variable for summary statistics:'),
     dcc.Dropdown(
         id='dropdown',
@@ -49,26 +61,33 @@ app.layout = html.Div([
 @app.callback(Output('display-value', 'figure'),
               [Input('dropdown', 'value')])
 def display_value(continuous_var):
-    grouped_mean=df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
+    grouped_mean=df.groupby(['Age Groups', 'Embarked'])[continuous_var].mean()
     results=pd.DataFrame(grouped_mean)
     # Create a grouped bar chart
     mydata1 = go.Bar(
-        x=results.loc['first'].index,
-        y=results.loc['first'][continuous_var],
-        name='First Class',
+        x=results.loc['1 children'].index,
+        y=results.loc['1 children'][continuous_var],
+        name='Children',
         marker=dict(color=color1)
     )
     mydata2 = go.Bar(
-        x=results.loc['second'].index,
-        y=results.loc['second'][continuous_var],
-        name='Second Class',
+        x=results.loc['2 young adult'].index,
+        y=results.loc['2 young adult'][continuous_var],
+        name='Young Adult',
         marker=dict(color=color2)
     )
     mydata3 = go.Bar(
-        x=results.loc['third'].index,
-        y=results.loc['third'][continuous_var],
-        name='Third Class',
+        x=results.loc['3 middle-aged'].index,
+        y=results.loc['3 middle-aged'][continuous_var],
+        name='Middle Aged',
         marker=dict(color=color3)
+    )
+
+    mydata4 = go.Bar(
+        x=results.loc['4 elderly'].index,
+        y=results.loc['4 elderly'][continuous_var],
+        name='Elderly',
+        marker=dict(color=color4)
     )
 
     mylayout = go.Layout(
@@ -77,7 +96,8 @@ def display_value(continuous_var):
         yaxis = dict(title = str(continuous_var)), # y-axis label
 
     )
-    fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
+    fig = go.Figure(data=[mydata1, mydata2, mydata3, mydata4], layout=mylayout)
+
     return fig
 
 
